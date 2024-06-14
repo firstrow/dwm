ed6acbec46452a06d44d0b5e970d7995830a361d
firstrow <firstrow@gmail.com>
2024-06-14 09:02:04 +0200

    datetime format changes

--- statusbar.sh
+++ statusbar.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 
 while : ; do
-    time="$(date +%I:%M\ %p)"
-    date="$(date +%-d-%-m-%Y\ %A)"
+    time="$(date +%H:%M)"
+    date="$(date +%d-%m-%Y\ %A)"
 
     xsetroot -name "[ ${date} ${time} ]"
 
